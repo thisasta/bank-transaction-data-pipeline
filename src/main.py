ec7ff3797fec60cb81eec86560a84d00c19b6c7a
Abishan Arankesan <abishan.aranke@outlook.de>
2026-06-09 16:45:57 +0200

    added DictReader

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import csv
 
 with open('data/raw/transactions.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader[1:]:  # Skip the header row
-        print(row)
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(row["transaction_id"], row["amount"], row["currency"])
